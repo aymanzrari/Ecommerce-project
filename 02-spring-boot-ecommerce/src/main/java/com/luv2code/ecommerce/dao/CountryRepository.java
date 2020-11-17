f7474b130b2fc7adf168b2da08c80328acc01ff5
ZRARI Ayman <AYMANZRARI@GMAIL.COM>
2020-11-17 19:14:41 +0100

    Initial commit

---
+++ 02-spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/CountryRepository.java
@@ -0,0 +1,15 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Country;
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "countries",path = "countries")
+public interface CountryRepository extends JpaRepository<Country,Integer> {
+}
